084d7ccbeaaf50b6a029c87681af983e84b3d6e4
DatBui305 <buianhdat305@gmail.com>
2025-03-10 09:53:04 +0700

    update

--- src/entities/message.entity.ts
+++ src/entities/message.entity.ts
@@ -1,4 +1,6 @@
+import { Admin } from 'src/entities/admin.entity';
 import { ChatRoom } from 'src/entities/chat-room';
+import { ChatStorage } from 'src/entities/chat-storage.entity';
 import {
   Entity,
   PrimaryGeneratedColumn,
@@ -6,6 +8,7 @@ import {
   ManyToOne,
   CreateDateColumn,
   UpdateDateColumn,
+  OneToOne,
 } from 'typeorm';
 @Entity({ name: 'messages' })
 export class Message {
@@ -15,6 +18,10 @@ export class Message {
   @Column()
   content: string;
 
+  @OneToOne(() => Admin, (admin) => admin.id)
+  sender: Admin;
+  @OneToOne(() => Admin, (admin) => admin.id)
+  receiver: Admin;
   @ManyToOne(() => ChatRoom, (room) => room.messages)
   room: ChatRoom;
 
